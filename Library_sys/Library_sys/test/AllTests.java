8c7cd706529cacc4d500faf0719c0fa07a7fc075
DanielYCouturier <DanielYCouturier@gmail.com>
2024-12-02 20:33:11 -0500

    suite

---
+++ Library_sys/Library_sys/test/AllTests.java
@@ -0,0 +1,6 @@
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+@Suite
+@SelectClasses({BookTest.class, LibraryTest.class, UserTest.class})
+public class AllTests {
+}
